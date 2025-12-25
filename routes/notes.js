cf804974bdf15d59fc1c0819cb2f1e32f45ac359
K Kondo <miraclexxxbassxxx@gmail.com>
2025-12-26 03:26:28 +0900

    update dog.js

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = "mongodb+srv://miraclexxxbassxxx_db_user:i4pmTt7EFoXsQiFr@cluster0.acnpaf6.mongodb.net/?appName=Cluster0@cluster0.acnpaf6.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
