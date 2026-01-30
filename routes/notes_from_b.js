1572a49c8ce3c3cfdf0b7bd09d85f89e734251fa
K Kondo <miraclexxxbassxxx@gmail.com>
2026-01-30 23:57:55 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var router = express.Router();
-const cors = require('cors');//corsミドルウェアを追加
+const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
+
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://miraclexxxbassxxx_db_user:i4pmTt7EFoXsQiFr@cluster0.acnpaf6.mongodb.net/?appName=Cluster0@cluster0.acnpaf6.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
 
 res.json(note);
 })
